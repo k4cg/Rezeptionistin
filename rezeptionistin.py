226f02783e828dad3a08234686ce1c556d5e46bc
Florian Baumann <flo@noqqe.de>
2015-05-28 11:02:46 +0200

    optional channels

--- rezeptionistin.py
+++ rezeptionistin.py
@@ -22,8 +22,8 @@ if not config.read("config.ini"):
 server=config.get('IRC','server')
 port=int(config.get('IRC', 'port'))
 nick=config.get('IRC', 'nick')
-ircchan=config.get('IRC', 'ircchan')
-debugchan=config.get('IRC', 'debugchan')
+ircchan=config.get('IRC', 'ircchan', fallback=None)
+debugchan=config.get('IRC', 'debugchan', fallback=None)
 useragent=config.get('HTTP', 'useragent')
 site = wiki.Wiki(config.get('MediaWiki', 'wikiapiurl'))
 site.login(config.get('MediaWiki', 'user'), config.get('MediaWiki', 'password'))
@@ -112,8 +112,10 @@ def on_privmsg(self, nick, host, message):
 
 # Start Bot
 irc.start()
-irc.join(ircchan)
-irc.join(debugchan)
+if ircchan:
+  irc.join(ircchan)
+if debugchan:
+  irc.join(debugchan)
 
 # Run Eventloop
 try:
