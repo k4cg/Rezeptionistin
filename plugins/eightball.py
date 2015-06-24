06794e09be8d1261616fa6fe63fa7c74c58c4f6a
Armin <armin@webhippie.de>
2015-06-24 21:56:25 +0200

    Eightball questions are being answered in query as well

--- plugins/eightball.py
+++ plugins/eightball.py
@@ -4,10 +4,19 @@ from plugin import Plugin
 class Eightball(Plugin):
   def help_text(self):
     return "fragen beantworten in Form: <soll/kann/darf/muss> ich * [<oder> *]"
-  def on_msg(self, bot, user_nick, host, channel, message):
+  
+  def answer(self, bot, msg, recipient, nick=None):
     keywords = ["kann ich", "darf ich", "soll ich", "muss ich"]
-    if any(message.lower().startswith(k) for k in keywords):
-      if "oder" in message:
-        bot.send_message(channel, user_nick + ": " + random.choice(list(open('lists/alternate_answers.txt'))))
+    if any(msg.lower().startswith(k) for k in keywords):
+      adressee = (nick + ": ") if nick else ""
+
+      if "oder" in msg:
+        bot.send_message(recipient, adressee + random.choice(list(open('lists/alternate_answers.txt'))))
       else:
-        bot.send_message(channel, user_nick + ": " + random.choice(list(open('lists/polar_answers.txt'))))
+        bot.send_message(recipient, adressee + random.choice(list(open('lists/polar_answers.txt'))))    
+  
+  def on_msg(self, bot, user_nick, host, channel, message):
+    self.answer(bot, message, channel, user_nick)
+
+  def on_privmsg(self, bot, user_nick, host, message):
+    self.answer(bot, message, user_nick)
