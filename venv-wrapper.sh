c079e713033834c9bd4b6a964d835890d1b83a0c
System administrator <root@nixe.k4cg.org>
2015-09-20 21:47:19 +0200

    removed unnecessary wrapper

--- venv-wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/run/current-system/sw/bin/bash
-cd /usr/local/rezeptionistin
-. /usr/local/rezeptionistin/.venv/bin/activate
-/usr/local/rezeptionistin/.venv/bin/python /usr/local/rezeptionistin/main.py -c /usr/local/rezeptionistin/config.ini 
