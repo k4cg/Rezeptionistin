aef11f437b38decedec60ff233a7495b23e389fe
System administrator <root@nixe.k4cg.org>
2015-09-29 09:07:25 +0200

    changed temperature ipv6

--- plugins/temperature.py
+++ plugins/temperature.py
@@ -11,7 +11,7 @@ class Temperature(Plugin):
 
   def on_msg(self, bot, user_nick, host, channel, message):
     if message.lower().startswith('!kt'):
-      temp = bot.netcat("2001:a60:f073:0:21a:92ff:fe50:bdfc", 31337, "9001").strip()
+      temp = bot.netcat("2001:a60:f073:0:76da:38ff:fe00:5e92", 31337, "9001").strip()
       
       station_id = "INUREMBE2";
       f = urllib2.urlopen('http://api.wunderground.com/api/a5744ceb15b96090/conditions/q/pws:' + station_id + '.json')
