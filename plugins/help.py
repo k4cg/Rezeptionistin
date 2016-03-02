391c14408cb0fcc118dd1ca0111ea3f84f91150d
Gama <gammenion@gmail.com>
2016-03-02 21:58:10 +0100

    updating help plugin

--- plugins/help.py
+++ plugins/help.py
@@ -6,7 +6,7 @@ class Help(Plugin):
       bot.send_message(user_nick, bot.translate("help_hello"))
       bot.send_message(user_nick, bot.translate("help_intro"))
       for plugin in bot.plugins:
-        text = plugin.help_text()
+        text = plugin.help_text(bot)
         if text != "":
           for t in text.split("\n"):
             bot.send_message(user_nick, t)
