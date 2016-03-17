b34a83bbf0470dbbcf3c1299a88a002c27bb3cdc
Gama <gammenion@gmail.com>
2016-03-17 20:29:37 +0100

    Adding lineart to private messages

--- plugins/lineart.py
+++ plugins/lineart.py
@@ -9,3 +9,8 @@ class Insult(Plugin):
     if message.lower().startswith('!lineart'):
       if len(message.split()) >= 1:
         bot.send_message(channel, random.choice(list(open('lists/lineart.txt'))))
+
+  def on_privmsg(self, bot, user_nick, host, message):
+    if message.lower().startswith('!lineart'):
+      if len(message.split()) >= 1:
+        bot.send_message(user_nick, random.choice(list(open('lists/lineart.txt'))))
