9e632e1be5fb2d2be144d92fd24fe1a73982a422
Corby <corby@c0rby.de>
2016-05-19 22:53:54 +0200

    Added a shakespear insulting plugin

---
+++ plugins/shakespear_insult.py
@@ -0,0 +1,28 @@
+import random
+from plugin import Plugin
+
+
+first_words = ["artless", "bawdy", "beslubberling", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissemblling", "droning", "errant", "fawning", "fobbing", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "mewling", "paunchy", "prlbbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "spongy", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"];
+
+middle_words = ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy~eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "iil-nurtured", "knotty~pated", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"];
+
+end_words = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "mistreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"];
+
+def create_insult():
+  return one(first_words) + ' ' + one(middle_words) + ' ' + one(end_words) + '.'
+
+def one(list):
+  return random.choice(list)
+
+class Insult(Plugin):
+  def help_text(self, bot):
+    return bot.translate("insult_shakespear_help")
+
+  def on_msg(self, bot, user_nick, host, channel, message):
+    if message.lower().startswith(bot.translate("insult_shakespear_cmd")):
+      insult = create_insult()
+      if len(message.split()) >= 2:
+        msg = message.split()[1] + ", thou " + insult
+      else:
+        msg = 'Thou ' + insult
+      bot.send_message(channel, msg)
