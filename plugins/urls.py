7c3a50a18e1e03100c3cb98fc5ad3fdca441ba3c
Guenther Schmitz <gpunktscripts@gpunktschmitz.com>
2017-01-28 17:26:34 +0100

    if title is empty set title to url

--- plugins/urls.py
+++ plugins/urls.py
@@ -8,11 +8,12 @@ class Urls(Plugin):
     if bot.httpregex.search(message.lower()) is not None:
       url = bot.geturlfrommsg(message)
       title = bot.sanitize(bot.geturltitle(url))
-      if not title == "":
-        if message.lower().startswith('!private') or message.lower().startswith('!pr'):
-          bot.send_message(channel, "[private] " + "Title: {title}".format(title=title), user_nick)
-        elif message.lower().startswith('!nsfw'):
-          bot.send_message(channel, "[nsfw] " + "Title: {title}".format(title=title), user_nick)
-        else:
-          bot.send_message(channel, "Title: {title}".format(title=title), user_nick)
-          print bot.get_plugin("MediaWiki").wikiupdate(title, url)
+      if title == "":
+        title = bot.geturlfrommsg(message)
+      if message.lower().startswith('!private') or message.lower().startswith('!pr'):
+        bot.send_message(channel, "[private] " + "Title: {title}".format(title=title), user_nick)
+      elif message.lower().startswith('!nsfw'):
+        bot.send_message(channel, "[nsfw] " + "Title: {title}".format(title=title), user_nick)
+      else:
+        bot.send_message(channel, "Title: {title}".format(title=title), user_nick)
+        print bot.get_plugin("MediaWiki").wikiupdate(title, url)
