0b3a61742956d911042d5655d7e69ed3314e1cf3
Florian Baumann <flo@noqqe.de>
2017-02-04 16:40:06 +0100

    keepalive failed

--- keepalive.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ERR=`journalctl --no-pager -u rezeptionistin --since $(date +%H:%m -d "1 hour ago") | grep -c "DEBUG:root:ERROR :Closing Link: nixe.k4cg.org"`
-
-if [ $ERR -ne 0 ]; then
-  systemctl restart rezeptionistin
-fi
