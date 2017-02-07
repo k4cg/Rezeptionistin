513cf60e9a7bea6926e277631dd4ed6c64d237f0
Florian Baumann <flo@noqqe.de>
2017-02-07 10:27:10 +0100

    !status plugin - fixes #31

---
+++ plugins/status.py
@@ -0,0 +1,25 @@
+# coding: utf8
+
+from plugin import Plugin
+
+class Status(Plugin):
+
+  def __init__(self, config=None):
+    super(Status, self).__init__()
+
+  def help_text(self, bot):
+    return bot.translate("status_help")
+
+  def on_msg(self, bot, user_nick, host, channel, message):
+    if message.startswith("!status") or message.startswith("!st"):
+        msg = bot.get_spacestatus_data()
+
+        if msg is None:
+          bot.send_message(channel, bot.translate("status_error"), user_nick)
+        else:
+          noise = str(msg['sound'])
+          temp = str(msg['temp'])
+          light = str(msg['light'])
+          hosts = str(msg['online'])
+
+          bot.send_message(channel, bot.translate("status_str1").format(noise=noise, temp=temp, light=light, hosts=hosts), user_nick)
