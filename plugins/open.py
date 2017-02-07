83731f6f9734bce4722422b89466cd7eec6748a6
Florian Baumann <flo@noqqe.de>
2017-02-07 10:26:58 +0100

    !status plugin - fixes #31

--- plugins/open.py
+++ plugins/open.py
@@ -3,13 +3,13 @@ import json
 import ConfigParser
 from plugin import Plugin
 
-class SpaceStatus(Plugin):
+class Open(Plugin):
 
   def __init__(self, config=None):
-    super(SpaceStatus, self).__init__()
+    super(Open, self).__init__()
 
   def help_text(self, bot):
-    return bot.translate("spacestatus_help")
+    return bot.translate("open_help")
 
   def on_msg(self, bot, user_nick, host, channel, message):
     if message.startswith("!offen") or message.startswith("!open"):
@@ -17,6 +17,6 @@ class SpaceStatus(Plugin):
         f = msg['online']
 
         if int(f) > 0:
-          bot.send_message(channel, bot.translate("spacestatus_str1").format(d=f), user_nick)
+          bot.send_message(channel, bot.translate("open_str1").format(d=f), user_nick)
         else:
-          bot.send_message(channel, bot.translate("spacestatus_str2").format(d=f), user_nick)
+          bot.send_message(channel, bot.translate("open_str2").format(d=f), user_nick)
