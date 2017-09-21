c07e704ee7dc2bac473ac59c9690e8b93e85fdbd
Florian Baumann <flo@noqqe.de>
2017-09-21 21:57:26 +0200

    New Door status

--- plugins/open.py
+++ plugins/open.py
@@ -14,9 +14,14 @@ class Open(Plugin):
   def on_msg(self, bot, user_nick, host, channel, message):
     if message.startswith("!offen") or message.startswith("!open"):
         msg = bot.get_spacestatus_data()
-        f = msg['online']
+        wifi = msg['online']
+        door = str(msg['hservierer']['door'])
 
-        if int(f) > 0:
-          bot.send_message(channel, bot.translate("open_str1").format(d=f), user_nick)
+        if int(wifi) > 0 and door == 'open':
+          bot.send_message(channel, bot.translate("open_str1").format(d=wifi,door=door), user_nick)
+        elif int(wifi) > 0 and door == 'closed':
+          bot.send_message(channel, bot.translate("open_str2").format(d=wifi,door=door), user_nick)
+        elif int(wifi) == 0 and door == 'open':
+          bot.send_message(channel, bot.translate("open_str2").format(d=wifi,door=door), user_nick)
         else:
-          bot.send_message(channel, bot.translate("open_str2").format(d=f), user_nick)
+          bot.send_message(channel, bot.translate("open_str3").format(d=wifi,door=door), user_nick)
